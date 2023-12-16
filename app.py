19e90a171bbb6506d58e12df3e7814221ac2d6b3
slimowert <pasha.starikoff@google.com>
2023-12-16 19:35:03 +0300

    Мама я наконецто починил докер, УРА, УРА, УРА!

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, url_for, request, redirect
+import os
+import json
+import logging
+
+import create_app, database
+from models import *
+
+
+app = create_app.create_app()
+logging.basicConfig(level=logging.DEBUG)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/select-category', methods=['POST', 'GET'])
+def select_category():
+    if request.method == 'POST':
+        url = request.form['category-url']
+        for cat_url in db.session.query(Category.url).distinct():
+            if url == cat_url[0]:
+                return "Сылка уже есть в списке"
+        os.system(f'python3 main.py {url}')
+        return redirect('/')
+
+    else:
+        categorys = database.get_all(Category)
+        return render_template("select-category.html", categorys=categorys)
+
+
+def category_id(id, char, idx_char):
+    category = database.get_instance(Category, id)
+
+    data = database.get_data(category)
+
+    keys = list()
+    for key in idx_char:
+        keys.append(char[int(key)])
+
+    data_list = list()
+
+    for item in data:
+        item_dict = json.loads(item[0])
+        data_item = list()
+        for key in keys:
+            if key in item_dict.keys():
+                data_item.append(item_dict[key])
+            else:
+                data_item.append("-")
+        data_list.append(data_item)
+
+    return render_template("category.html", category=category, table_head=keys, data_list=data_list)
+
+
+@app.route('/select-displayed-characteristics/<int:id>', methods=['POST', 'GET'])
+def select_displayed_characteristics(id):
+    if request.method == 'POST':
+        # print(request.form.getlist('check'))
+        category = database.get_instance(Category, id)
+        char = category.characteristics.split(', ')
+        return category_id(id, char, request.form.getlist('check'))
+        
+    else:
+        category = database.get_instance(Category, id)
+        char = category.characteristics.split(', ')
+        return render_template('select-displayed-characteristics.html', category=category, char=char)
+
+
+@app.route('/faqs')
+def faqs():
+    return render_template('faqs.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=50001, debug=True)
---
+++ main.py
@@ -0,0 +1,117 @@
+import sys
+import time
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+import logging
+
+from cookie import get_cookies, headers
+import database
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def imformation_about_category(url, cookies):
+    response = requests.get(url, cookies=cookies, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    name = soup.find("div", class_="products-page").find('h1', class_="title").text
+    category = dict()
+    category["name"] = name
+    category['url'] = url
+    return category
+
+
+def get_all_urls_of_category(url, cookies):
+    logging.info("Формируется лист ссылок категории")
+    urls = []
+    next_page = url
+    while len(next_page) > 0:
+        response = requests.get(next_page, cookies=cookies, headers=headers)
+        soup = BeautifulSoup(response.text, 'lxml')
+        urls.extend(get_urls_from_page(soup))
+        try:
+            next_page = soup.find('button', class_='pagination-widget__show-more-btn')
+            next_page = URL + next_page.get('data-url')
+        except AttributeError:
+            return urls
+    
+    return urls
+
+
+def get_urls_from_page(soup):
+    elements = soup.find_all('a', class_="catalog-product__name ui-link ui-link_black") 
+    return list(map(lambda element: URL + element.get("href") + 'characteristics/', elements))
+
+
+def get_notebook_data(url, cookies):
+    notebook = dict()
+    
+    response = requests.get(
+        url,
+        cookies=cookies,
+        headers=headers,
+    )
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    characteristic = soup.find("div", class_="product-characteristics-content")
+    groups = characteristic.find_all('div', class_='product-characteristics__group')
+    for group in groups:
+        specs = group.find_all('div', class_='product-characteristics__spec')
+        for spec in specs:
+            titles = spec.find('div', class_='product-characteristics__spec-title').text
+            value = spec.find('div', class_='product-characteristics__spec-value').text
+            notebook[titles.strip()] = value.strip()
+
+
+    if old_price_element := soup.find('span', class_='product-buy__prev'):
+        notebook["price"], notebook["price_without_discount"] = map(int, soup.find('div',class_='product-buy__price product-buy__price_active').text.replace(' ', '').split('₽'))
+        notebook["price_without_discount"] = int(old_price_element.text.replace(' ', ''))
+        notebook["discount"] = round(100 - (notebook["price"] / notebook["price_without_discount"]) * 100)
+    elif price := soup.find('div', class_='product-buy__price'):
+        notebook["price"] = int(price.text.replace(' ', '')[:-1])
+        notebook["price_without_discount"], notebook["discount"] = 0, 0
+    
+    notebook["url"] = url
+
+    return notebook
+
+
+def parser(url):
+    start_time = time.time()
+
+    cookies=get_cookies(URL)
+
+    category = imformation_about_category(url, cookies)
+    urls = get_all_urls_of_category(url, cookies)
+
+    max_len_dict = dict()
+    name_table = category["name"].replace(' ', '_')
+    database.delete_table(name_table)
+    database.create_table(name_table)
+    for url in tqdm(urls, ncols=70, unit='item', colour='green', file=sys.stdout):
+        item = get_notebook_data(url , cookies)
+        if len(item) > len(max_len_dict):
+            max_len_dict = item
+        database.add_instance(name_table, item)
+
+    category['characteristics'] = ', '.join(map(str, max_len_dict.keys()))
+    database.add_category(category)
+
+    total_time = time.time() - start_time
+    logging.info(f"Время выполнения:\n"
+          f"{(total_time // 3600):02.0f}:"
+          f"{(total_time % 3600 // 60):02.0f}:"
+          f"{(total_time % 60):02.0f}")
+
+
+if __name__ == '__main__':
+    URL = 'https://www.dns-shop.ru'
+    parser(sys.argv[1])
+    # parser('https://www.dns-shop.ru/catalog/17a9dcd816404e77/instrumenty-dlya-vitoj-pary/')
+    # parser('https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/')
+    # parser('https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/')
+    # parser('https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/')
+    # parser('https://www.dns-shop.ru/catalog/17a8ae4916404e77/televizory/')
+    # parser('https://www.dns-shop.ru/catalog/8af402e76a6db2e5/elektrosamokaty/')
+    # parser('https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/')  очень долго
